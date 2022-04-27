4a538cc74842bd6312a98a86b910712d5452a32a
pinheirogh <gabriel.pinheiroconceicao@gmail.com>
2022-04-27 18:38:49 -0300

    Traduz termos matemáticos usados nas funções ao Portugues

--- aritmetica.py
+++ aritmetica.py
@@ -1,21 +1,20 @@
-def gcd(a, b):
-    # Return the GCD of a and b using Euclid's Algorithm
+def mdc(a, b):
+    # Retorna o MDC de 'a' e 'b' usando o algoritmo de Euclides
     while a != 0:
         a, b = b % a, a
     return b
 
 
-def findModInverse(a, m):
-    # Returns the modular inverse of a % m, which is
-    # the number x such that a*x % m = 1
+def inverso_modular(a, m):
+    # Retorna o inverso modular de 'a' em 'm' equivalente a a*x % m = 1
 
-    if gcd(a, m) != 1:
-        return None # no mod inverse if a & m aren't relatively prime
+    if mdc(a, m) != 1:
+        return None 
 
-    # Calculate using the Extended Euclidean Algorithm:
+    # Algoritmo de Euclides Estendido
     u1, u2, u3 = 1, 0, a
     v1, v2, v3 = 0, 1, m
     while v3 != 0:
-        q = u3 // v3 # // is the integer division operator
+        q = u3 // v3 
         v1, v2, v3, u1, u2, u3 = (u1 - q * v1), (u2 - q * v2), (u3 - q * v3), v1, v2, v3
     return u1 % m
