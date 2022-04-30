edbae47cb9f86fbb8ca8f95da76552dad8998a30
Gabriel Pinheiro <gabriel.pinheiroconceicao@gmail.com>
2022-04-30 14:17:39 -0300

    Adiciona criptografia AES funcional

--- AES.py
+++ AES.py
@@ -1,17 +1,94 @@
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
+from Crypto.Random import get_random_bytes
 import random
+import base64
+from criptografia_RSA import codificar_base64, decodificar_base64
 
+def main():    
+    opcao = input('''1 - Criptografar\n2 - Descriptografar\nOpção: ''')
 
-key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
-iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])
+    if opcao == '1':
+        nome_arquivo_mensagem = 'arquivos_texto/' + input('Digite o nome do arquivo de mensagem: ') + '.txt'
 
+        # Abrir arquivo para leitura de texto
+        with open(nome_arquivo_mensagem, 'r') as arquivo:
+            mensagem = arquivo.read()
+        # Fechar arquivo
+        arquivo.close()
 
-counter = Counter.new(128, initial_value=int(iv,16))
+        mensagem_enc, chave_sessao, iv = encrypt(mensagem)
 
+        nome_arquivo_cifrado = 'arquivos_texto/msg_cifrada_AES.txt'
+        conteudo_cifrado = base64.b64encode(mensagem_enc)
 
-cipher=AES.new(key, AES.MODE_CTR, counter=counter)
+        file_object = open(nome_arquivo_cifrado, 'wb')
+        file_object.write(conteudo_cifrado)
+        file_object.close()
 
-mensagem = 'Teste de mensagem'
+        nome_arquivo_chavesessao = 'arquivos_texto/chave_sessao_AES.txt'
+        chave_64 = base64.b64encode(chave_sessao)
 
-mensagem_enc = cipher.encrypt(mensagem)
+        conteudo_cifrado = chave_64
+        file_object = open(nome_arquivo_chavesessao, 'wb')
+        file_object.write(conteudo_cifrado)
+        file_object.close()
+
+        nome_arquivo_iv = 'arquivos_texto/iv_AES.txt'
+        conteudo_cifrado = iv
+        file_object = open(nome_arquivo_iv, 'w')
+        file_object.write(conteudo_cifrado)
+        file_object.close()
+        
+    elif opcao == '2':
+        nome_arquivo_cifrado = 'arquivos_texto/' + input('Digite o nome do arquivo de mensagem cifrada: ') + '.txt'
+        nome_arquivo_chavesessao = 'arquivos_texto/' + input('Digite o nome do arquivo de chave: ') + '.txt'
+        nome_arquivo_iv = 'arquivos_texto/' + input('Digite o nome do arquivo de iv: ') + '.txt'
+
+        # Abrir arquivo para leitura de texto
+        with open(nome_arquivo_cifrado, 'r') as arquivo:
+            mensagem_cifrada = arquivo.read()
+        # Fechar arquivo
+        arquivo.close()
+
+        mensagem_cifrada = base64.b64decode(mensagem_cifrada)
+
+        # Abrir arquivo para leitura de texto
+        with open(nome_arquivo_chavesessao, 'r') as arquivo:
+            chave_sessao = arquivo.read()
+        # Fechar arquivo
+        arquivo.close()
+
+        chave_sessao = base64.b64decode(chave_sessao)
+
+        # Abrir arquivo para leitura de texto
+        with open(nome_arquivo_iv, 'r') as arquivo:
+            iv = arquivo.read()
+        # Fechar arquivo
+        arquivo.close()
+
+        print()
+        print('MENSAGEM DECIFRADA:')
+        print(decrypt(mensagem_cifrada, chave_sessao, iv))
+
+def encrypt(mensagem):
+    chave_sessao = get_random_bytes(16)
+    iv = ''.join([str(random.randint(0, 0xFF)) for i in range(16)])
+    counter = Counter.new(128, initial_value=int(iv, 16))
+
+    cifracao = AES.new(chave_sessao, AES.MODE_CTR, counter=counter)
+
+    mensagem_enc = cifracao.encrypt(mensagem)
+
+    return mensagem_enc, chave_sessao, iv
+
+def decrypt(mensagem_enc, chave_sessao, iv):
+
+    counter = Counter.new(128, initial_value=int(iv, 16))
+    decifracao = AES.new(chave_sessao, AES.MODE_CTR, counter=counter)
+    mensagem_dec = str(decifracao.decrypt(mensagem_enc)).replace('b\'', '').replace('\'', '')
+
+    return mensagem_dec
+
+if __name__ == '__main__':
+    main()
